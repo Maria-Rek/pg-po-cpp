f122498fdcf30260be34c9560bb7b32e31d1f388
Maria-Rek <maria.rek04@gmail.com>
2025-04-06 19:29:20 +0200

    main

--- main.cpp
+++ main.cpp
@@ -19,22 +19,30 @@
 
 int main() {
     system("chcp 65001");
-
     std::srand(static_cast<unsigned>(std::time(nullptr)));
 
-    Swiat swiat(10, 10);  // Plansza 10x10 (Człowiek na środku)
+    Swiat swiat(10, 10);
 
+    //Rośliny
+    swiat.dodajOrganizm(new Trawa(&swiat, Punkt(1, 1)));
+    swiat.dodajOrganizm(new Trawa(&swiat, Punkt(8, 2)));
+    swiat.dodajOrganizm(new Mlecz(&swiat, Punkt(5, 5)));
+    swiat.dodajOrganizm(new Guarana(&swiat, Punkt(3, 4)));
+    swiat.dodajOrganizm(new WilczeJagody(&swiat, Punkt(2, 8)));
+    swiat.dodajOrganizm(new BarszczSosnowskiego(&swiat, Punkt(6, 2)));
+
+    //Zwierzęta
     swiat.dodajOrganizm(new Wilk(&swiat, Punkt(4, 4)));
+    swiat.dodajOrganizm(new Wilk(&swiat, Punkt(7, 3)));
     swiat.dodajOrganizm(new Owca(&swiat, Punkt(6, 4)));
+    swiat.dodajOrganizm(new Owca(&swiat, Punkt(1, 5)));
     swiat.dodajOrganizm(new Lis(&swiat, Punkt(2, 2)));
     swiat.dodajOrganizm(new Zolw(&swiat, Punkt(3, 6)));
     swiat.dodajOrganizm(new Antylopa(&swiat, Punkt(7, 7)));
-    swiat.dodajOrganizm(new Trawa(&swiat, Punkt(5, 1)));
-    swiat.dodajOrganizm(new Guarana(&swiat, Punkt(4, 5)));
-    swiat.dodajOrganizm(new Mlecz(&swiat, Punkt(5, 5)));
-    swiat.dodajOrganizm(new WilczeJagody(&swiat, Punkt(6, 6)));
-    swiat.dodajOrganizm(new BarszczSosnowskiego(&swiat, Punkt(2, 4)));
-    swiat.dodajOrganizm(new CyberOwca(&swiat, Punkt(1, 1)));
+    swiat.dodajOrganizm(new Antylopa(&swiat, Punkt(6, 8)));
+    swiat.dodajOrganizm(new CyberOwca(&swiat, Punkt(0, 9)));
+
+
     swiat.dodajOrganizm(new Czlowiek(&swiat, Punkt(swiat.getSzerokosc() / 2, swiat.getWysokosc() / 2)));
 
 
